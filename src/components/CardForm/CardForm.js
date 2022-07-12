a331d184915bb04c59c5c2dc2222b00fc03f59c4
Krzysztof Mirończuk <krzysztof.mironczuk@socart.media>
2022-07-12 08:48:21 +0200

    Redux add card

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,22 +1,26 @@
 import styles from './CardForm.modules.scss';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 
 const CardForm = (props) => {
+
+  const dispatch = useDispatch();
+
   const [title, setTitle] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch({type: 'ADD_CARD', payload: {title, columnId:props.columnId}});
 
-    props.addCard({ title: title }, props.columnId);
     setTitle('');
   };
 
   return (
     <form onSubmit={handleSubmit} className={styles.CardForm}>
       <div>
-        <TextInput type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <TextInput type="text" value={title}  onChange={(e) => setTitle(e.target.value)} />
       </div>
       <div>
         <Button>Add card</Button>
--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,21 +1,26 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
 
+
 const ColumnForm = (props) => {
+
+  const dispatch = useDispatch();
+
   const [title, setTitle] = useState('');
   const [icon, setIcon] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleChange = (e) => {
     e.preventDefault();
-    props.action({ title: title, icon: icon });
+    dispatch({type: 'ADD_COLUMN', payload: { title, icon}});
     setTitle('');
     setIcon('');
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.columnForm}>
+    <form onSubmit={handleChange} className={styles.columnForm}>
       <div>
         <label>Title: </label> <TextInput type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
       </div>
